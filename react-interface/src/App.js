ae8b1d7b916d6fbe8703604d5459588e336531d6
Evan Zhao <zevany16@gmail.com>
2022-11-04 17:53:10 -0700

    Addition of images directory
    Added images directory to src to reduce file clutter and increase organzation.

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import logo from './logo192.png'
+import logo from './images/logo192.png'
 
 export default function App() {
 
