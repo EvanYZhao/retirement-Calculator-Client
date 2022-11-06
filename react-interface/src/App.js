d266bff1e5ecbdeab646314c7cb59bc393012362
Evan Zhao <zevany16@gmail.com>
2022-11-05 17:34:01 -0700

    Rendered API data to React Page
    Created conditional load checking to make sure object wasn't being rendered to page before it loaded in. Modified profileData state to be an array of objects in order to make load-checking work. Passed loaded data as a prop to another React component for page display.

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -4,16 +4,29 @@ import UserForm from './components/userForm'
 
 export default function App() {
 
-  const [profileData, setProfileData] = useState(null)
-  const [name, setName] = useState('')
-  const [retirementBalance, setRetirementBalance] = useState(0)
-  const [yearlyExpenses, setYearlyExpenses] = useState(0)
-  const [years, setYears] = useState(0)
-  const [stockPercentage, setStockPercentage] = useState(0)
+  //profileData created as an array so conditional load checking is possible (profileData.length > 0?)
+  const [profileData, setProfileData] = useState([])
+  // const [name, setName] = useState('')
+  // const [retirementBalance, setRetirementBalance] = useState(0)
+  // const [yearlyExpenses, setYearlyExpenses] = useState(0)
+  // const [years, setYears] = useState(0)
+  // const [stockPercentage, setStockPercentage] = useState(0)
+
+  useEffect(() => {
+    axios.get("/profile")
+    .then(res => {
+    setProfileData([{
+      "name": res.data.name,
+      "retirementBalance": res.data.retirement_account_balance,
+      "stockPercentage": res.data.stock_percentage,
+      "yearly_expenses": res.data.yearly_expenses,
+      "years": res.data.years}])
+  })
+  }, [])
 
   return (
     <div className="App">
-      <UserForm />
+      <UserForm object={profileData}/>
     </div>
   );
 }
--- react-interface/src/components/userForm.js
+++ react-interface/src/components/userForm.js
@@ -1,6 +1,26 @@
 import React from 'react'
 
-export default function UserForm() {
+export default function UserForm({object}) {
+
+  //Conditional Load that makes sure objects are loaded in before rendering to page
+  function displayData(){
+    if (object.length > 0){
+      return(
+        <div>
+          <p>My name is {object[0].name}</p>
+          <p>My projected retirement account balance is ${object[0].retirementBalance}</p>
+          <p>I spend about ${object[0].yearly_expenses} per year</p>
+          <p>I need this money to last me {object[0].years} years</p>
+          <p>I invest about {object[0].stockPercentage}% of my money into stocks</p>
+        </div>
+      )
+    }
+    else {
+      return <h1>Loading...</h1>
+    }
+
+  }
+
   return (
     <div className="form-container">
         <h2>Projected Retirement Account Statistics</h2>
@@ -29,6 +49,7 @@ export default function UserForm() {
             required/>
             <button>Submit</button>
         </form>
+        {displayData()}
     </div>
   )
 }
