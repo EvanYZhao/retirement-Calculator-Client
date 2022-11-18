4c4406541c05eb4ecb1387f5cf5d09bfb8afdb31
Evan Zhao <zevany16@gmail.com>
2022-11-18 01:21:34 -0800

    Random

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useEffect}  from 'react'
 import UserForm from './components/userForm'
 import UserResults from './components/userResults'
 
 
 export default function App() {
+
+  //Put a useEffect here to call a function that clears cookies on page refresh
+  
   return (
     <div className="App">
       <UserForm />
--- react-interface/src/components/userForm.js
+++ react-interface/src/components/userForm.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import '../styling/formStyle.css'
 
 const initialFormData = {
